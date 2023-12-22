03f5f4c67ede66dcd461d9e7036673f9009e2295
Gui Mendel <31701583+guiMendel@users.noreply.github.com>
2023-12-21 22:56:26 -0300

    ✨ Stances

--- Assets/Scripts/Samurai.cs
+++ Assets/Scripts/Samurai.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -29,11 +30,60 @@ public class Samurai : MonoBehaviour
   private void Update()
   {
     ApplyMovement();
+
+    UpdateTransitionTimer();
+  }
+
+
+  // === DASH
+  [Header("Dash")]
+
+  [Tooltip("Seconds it takes the samurai to enter dash stance")]
+  public float dashStanceWindup = 0.3f;
+
+  [Tooltip("Seconds it takes the samurai to enter dash stance")]
+  public float dashStanceUnwind = 0.2f;
+
+  enum Stance { Idle, TransitionIn, TransitionOut, Dash }
+
+  // Current stance of the samurai
+  Stance currentStance = Stance.Idle;
+
+  // Current coroutine for stance transition
+  Coroutine stanceTransitionCoroutine;
+
+  // How many seconds a transition has been going for
+  float transitionDuration = -1f;
+
+  void UpdateTransitionTimer()
+  {
+    if (transitionDuration >= 0) transitionDuration += Time.deltaTime;
+  }
+
+  IEnumerator SetDashStance(bool enterDashStance, float modifier = 0f)
+  {
+    print("modifier " + modifier);
+
+    transitionDuration = 0f;
+
+    currentStance = enterDashStance ? Stance.TransitionIn : Stance.TransitionOut;
+
+    GetComponent<SpriteRenderer>().color = enterDashStance ? Color.blue : Color.gray;
+
+    if (modifier > 0f)
+      yield return new WaitForSeconds(
+        (enterDashStance ? dashStanceWindup : dashStanceUnwind) * modifier);
+
+    GetComponent<SpriteRenderer>().color = enterDashStance ? Color.red : Color.white;
+
+    currentStance = enterDashStance ? Stance.Dash : Stance.Idle;
+
+    transitionDuration = -1f;
   }
 
 
   // === MOVEMENT
-  [Header("MOVEMENT")]
+  [Header("Movement")]
 
   [Tooltip("Speed of walking")]
   public float walkSpeed = 3f;
@@ -53,13 +103,9 @@ public class Samurai : MonoBehaviour
 
     float newDistance = OpponentDistance - moveDirection * walkSpeed * Time.deltaTime;
 
-    print("before " + newDistance);
-
     // Respect distance boundaries
     newDistance = Helper.UnsignedClamp(
       newDistance, orchestrator.minSamuraisDistance, orchestrator.maxSamuraisDistance);
-
-    print("after " + newDistance);
 
     transform.position = new Vector2(opponent.transform.position.x - newDistance, transform.position.y);
   }
@@ -79,5 +125,39 @@ public class Samurai : MonoBehaviour
 
     // Stop movement when released
     else if (value.phase != InputActionPhase.Started) Halt();
+  }
+
+  public void Sword(InputAction.CallbackContext value)
+  {
+    float modifier = 1f;
+
+    void Interrupt(float currentStanceDuration)
+    {
+      if (stanceTransitionCoroutine != null)
+      {
+        // If in the middle of an unwind, discount time
+        if (transitionDuration > 0f)
+          modifier = Mathf.Max(transitionDuration / currentStanceDuration, 0.2f);
+
+        StopCoroutine(stanceTransitionCoroutine);
+      }
+    }
+
+    // Enter dash on press
+    if (value.performed && currentStance != Stance.TransitionIn)
+    {
+      Interrupt(dashStanceUnwind);
+
+      stanceTransitionCoroutine = StartCoroutine(SetDashStance(true, modifier));
+      return;
+    }
+
+    // Leave dash stance on release
+    if (value.canceled && currentStance != Stance.TransitionOut)
+    {
+      Interrupt(dashStanceWindup);
+
+      stanceTransitionCoroutine = StartCoroutine(SetDashStance(false, modifier));
+    }
   }
 }
